b8e053612e4efe3722e4051ab92bbae57cb83405
jonchou <jonchou@ucdavis.edu>
2015-10-30 01:43:47 -0700

    Initialize Login View Controller storyboard

---
+++ OnTheMap/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  OnTheMap
+//
+//  Created by Jonathan Chou on 10/30/15.
+//  Copyright © 2015 Jonathan Chou. All rights reserved.
+//
+
+import UIKit
+
+
+class LoginViewController: UIViewController {
+    
+    var session: NSURLSession!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        /* Get the shared URL session */
+        session = NSURLSession.sharedSession()
+        
+        /* Configure the UI */
+    //    self.configureUI()
+    }
+
+    
+}
