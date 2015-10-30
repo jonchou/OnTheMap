4823a2900337dad52d0ca7cf67f906c0b455ad23
jonchou <jonchou@ucdavis.edu>
2015-10-30 02:11:49 -0700

    Add segue to MapViewController

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -23,5 +23,23 @@ class LoginViewController: UIViewController {
     //    self.configureUI()
     }
 
+    @IBAction func loginButtonTouch(sender: AnyObject) {
+        /*TMDBClient.sharedInstance().authenticateWithViewController(self) { (success, errorString) in
+            if success {
+                self.completeLogin()
+            } else {
+                self.displayError(errorString)
+            }
+        }
+        */
+        completeLogin()
+    }
+    
+    func completeLogin() {
+        dispatch_async(dispatch_get_main_queue(), {
+            let controller = self.storyboard!.instantiateViewControllerWithIdentifier("MapViewController")// as! UINavigationController
+            self.presentViewController(controller, animated: true, completion: nil)
+        })
+    }
     
 }
