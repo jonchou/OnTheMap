0741aaa4ea4948d3d3dabd2dff2c148a8682f3f1
jonchou <jonchou@ucdavis.edu>
2015-10-30 16:56:54 -0700

    Add keyboard recognizer

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -13,6 +13,15 @@ class LoginViewController: UIViewController {
     
     var session: NSURLSession!
     
+    var tapRecognizer: UITapGestureRecognizer? = nil
+    var keyboardAdjusted = false
+    var lastKeyboardOffset : CGFloat = 0.0
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var debugTextLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,7 +29,21 @@ class LoginViewController: UIViewController {
         session = NSURLSession.sharedSession()
         
         /* Configure the UI */
-    //    self.configureUI()
+        self.configureUI()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.addKeyboardDismissRecognizer()
+        self.subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        self.removeKeyboardDismissRecognizer()
+        self.unsubscribeToKeyboardNotifications()
     }
 
     @IBAction func loginButtonTouch(sender: AnyObject) {
@@ -32,7 +55,30 @@ class LoginViewController: UIViewController {
             }
         }
         */
-        completeLogin()
+        
+        if usernameTextField.text!.isEmpty {
+            debugTextLabel.text = "Username Empty."
+        } else if passwordTextField.text!.isEmpty {
+            debugTextLabel.text = "Password Empty."
+        } else {
+            
+            /*
+            Steps for Authentication...
+            https://www.themoviedb.org/documentation/api/sessions
+            
+            Step 1: Create a new request token
+            Step 2: Ask the user for permission via the API ("login")
+            Step 3: Create a session ID
+            
+            Extra Steps...
+            Step 4: Go ahead and get the user id ;)
+            Step 5: Got everything we need, go to the next view!
+            
+            */
+        //    self.getRequestToken()
+            completeLogin()
+        }
+
     }
     
     func completeLogin() {
@@ -42,4 +88,102 @@ class LoginViewController: UIViewController {
         })
     }
     
+}
+
+extension LoginViewController {
+    
+    func configureUI() {
+/*
+        /* Configure background gradient */
+        self.view.backgroundColor = UIColor.clearColor()
+        let colorTop = UIColor(red: 0.345, green: 0.839, blue: 0.988, alpha: 1.0).CGColor
+        let colorBottom = UIColor(red: 0.023, green: 0.569, blue: 0.910, alpha: 1.0).CGColor
+        self.backgroundGradient = CAGradientLayer()
+        self.backgroundGradient!.colors = [colorTop, colorBottom]
+        self.backgroundGradient!.locations = [0.0, 1.0]
+        self.backgroundGradient!.frame = view.frame
+        self.view.layer.insertSublayer(self.backgroundGradient!, atIndex: 0)
+        
+        /* Configure header text label */
+        headerTextLabel.font = UIFont(name: "AvenirNext-Medium", size: 24.0)
+        headerTextLabel.textColor = UIColor.whiteColor()
+        
+        /* Configure email textfield */
+        let emailTextFieldPaddingViewFrame = CGRectMake(0.0, 0.0, 13.0, 0.0);
+        let emailTextFieldPaddingView = UIView(frame: emailTextFieldPaddingViewFrame)
+        usernameTextField.leftView = emailTextFieldPaddingView
+        usernameTextField.leftViewMode = .Always
+        usernameTextField.font = UIFont(name: "AvenirNext-Medium", size: 17.0)
+        usernameTextField.backgroundColor = UIColor(red: 0.702, green: 0.863, blue: 0.929, alpha:1.0)
+        usernameTextField.textColor = UIColor(red: 0.0, green:0.502, blue:0.839, alpha: 1.0)
+        usernameTextField.attributedPlaceholder = NSAttributedString(string: usernameTextField.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor.whiteColor()])
+        usernameTextField.tintColor = UIColor(red: 0.0, green:0.502, blue:0.839, alpha: 1.0)
+        
+        /* Configure password textfield */
+        let passwordTextFieldPaddingViewFrame = CGRectMake(0.0, 0.0, 13.0, 0.0);
+        let passwordTextFieldPaddingView = UIView(frame: passwordTextFieldPaddingViewFrame)
+        passwordTextField.leftView = passwordTextFieldPaddingView
+        passwordTextField.leftViewMode = .Always
+        passwordTextField.font = UIFont(name: "AvenirNext-Medium", size: 17.0)
+        passwordTextField.backgroundColor = UIColor(red: 0.702, green: 0.863, blue: 0.929, alpha:1.0)
+        passwordTextField.textColor = UIColor(red: 0.0, green:0.502, blue:0.839, alpha: 1.0)
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: passwordTextField.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor.whiteColor()])
+        passwordTextField.tintColor = UIColor(red: 0.0, green:0.502, blue:0.839, alpha: 1.0)
+        
+        /* Configure debug text label */
+        headerTextLabel.font = UIFont(name: "AvenirNext-Medium", size: 20)
+        headerTextLabel.textColor = UIColor.whiteColor()
+ */
+        /* Configure tap recognizer */
+        tapRecognizer = UITapGestureRecognizer(target: self, action: "handleSingleTap:")
+        tapRecognizer?.numberOfTapsRequired = 1
+        
+    }
+}
+
+extension LoginViewController {
+    
+    func addKeyboardDismissRecognizer() {
+        view.addGestureRecognizer(tapRecognizer!)
+    }
+    
+    func removeKeyboardDismissRecognizer() {
+        view.removeGestureRecognizer(tapRecognizer!)
+    }
+    
+    func handleSingleTap(recognizer: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeToKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        
+        if keyboardAdjusted == false {
+            lastKeyboardOffset = getKeyboardHeight(notification) / 2
+            view.superview?.frame.origin.y -= lastKeyboardOffset
+            keyboardAdjusted = true
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        
+        if keyboardAdjusted == true {
+            view.superview?.frame.origin.y += lastKeyboardOffset
+            keyboardAdjusted = false
+        }
+    }
+    
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.CGRectValue().height
+    }
 }
