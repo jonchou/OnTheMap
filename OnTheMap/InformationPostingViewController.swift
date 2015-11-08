384c27fee431e121592d9ebb9ac7b0101c84c505
jonchou <jonchou@ucdavis.edu>
2015-11-07 20:43:40 -0800

    Add Information Posting View

---
+++ OnTheMap/InformationPostingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InformationPostingViewController.swift
+//  OnTheMap
+//
+//  Created by Jonathan Chou on 11/7/15.
+//  Copyright © 2015 Jonathan Chou. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class InformationPostingViewController: UIViewController {
+    
+    @IBAction func leaveView(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+}
