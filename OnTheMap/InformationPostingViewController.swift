920d22de383ec76efce816977f1713929bde8674
jonchou <jonchou@ucdavis.edu>
2015-11-07 22:43:50 -0800

    Add geocoding

--- OnTheMap/InformationPostingViewController.swift
+++ OnTheMap/InformationPostingViewController.swift
@@ -8,11 +8,39 @@
 
 import Foundation
 import UIKit
+import MapKit
 
 class InformationPostingViewController: UIViewController {
+    
+    @IBOutlet weak var studentLocation: UITextField!
+    @IBOutlet weak var mapView: MKMapView!
     
     @IBAction func leaveView(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
     }
     
+    @IBAction func geoCodeLocation(sender: AnyObject) {
+        let geoCoder =  CLGeocoder()
+        
+        // ConfigureUI
+        configureUI()
+        geoCoder.geocodeAddressString(studentLocation.text!, completionHandler:
+            {(placemarks, error) -> Void in
+            if let placemark = placemarks?.first {
+                let coordinates:CLLocationCoordinate2D = placemark.location!.coordinate
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = coordinates
+                self.mapView.addAnnotation(annotation)
+                // zoom in on region
+                let mySpan = MKCoordinateSpanMake(1.0, 1.0)
+                let myRegion = MKCoordinateRegionMake(coordinates, mySpan)
+                self.mapView.region = myRegion
+            }
+        })
+    }
+    
+    func configureUI() {
+        //TODO: configure hiding UI
+        mapView.hidden = false
+    }
 }
